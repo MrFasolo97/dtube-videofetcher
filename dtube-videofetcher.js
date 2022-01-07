806a05a0a43f471f4e8acf0f3ca6a60d34c8771a
MrFasolo97 <79fasolo97@gmail.com>
2022-01-07 21:27:52 +0100

    Added the function to look for originaldtubers requests filtering out already whitelisted users

--- dtube-videofetcher.js
+++ dtube-videofetcher.js
@@ -2,27 +2,35 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const fs = require('fs');
 const { Command } = require('commander');
+const restClient = require('sync-rest-client');
+const _ = require("lodash");
 
 
 async function fetcher(agg, config) {
   const url = config.mongo_address;
 
   var mongo_client = new MongoClient(url);
-  var r = [];
   await mongo_client.connect();
   const mongoDB = mongo_client.db();
   const contents_collection = mongoDB.collection("contents");
   var response = await contents_collection.aggregate(agg).toArray();
+  await mongo_client.close();
+  return response;
+}
+
+async function link_fetcher(agg, config) {
+  var r = [];
+  var response = await fetcher(agg, config);
   for (var i=0; i<response.length; i++) {
-    r.push('https://d.tube/v/'+response[i]._id);
+    r.push('https://d.tube/v/' + response[i]._id);
   }
-  mongo_client.close();
   return r;
 }
 
-function filter(tag, options) {
+async function filter(tag, options) {
+  var agg = false;
   if(tag != null) {
-    var agg = [
+    agg = [
       {
         '$match': {
           '$and': [
@@ -36,15 +44,13 @@ function filter(tag, options) {
       }
     ];
     agg[0]["$match"]["$and"].push({['tags.'+tag]: { '$gt': 0 }});
-    return agg;
-  } else {
-    return false;
   }
+  return agg;
 }
 
 async function main(options) {
   const config_file = fs.readFileSync("./config.json");
-  const config = JSON.parse(config_file); 
+  const config = JSON.parse(config_file);
 
   if(options.onelove) {
     const agg1 = [
@@ -72,16 +78,32 @@ async function main(options) {
         }
       }
     ];
-    console.log(JSON.stringify(await fetcher(agg1, config)));
+    console.log(JSON.stringify(await link_fetcher(agg1, config)));
   }
-
+  if(options.original) {
+    var agg4 = await filter("originaldtuber", options);
+    const originaldtubers = await restClient.get("https://dtube.fso.ovh/oc/creators").body;
+    if (agg4 !== false) {
+      var list = await fetcher(agg4, config);
+      for (var i=0; i<list.length; i++) {
+        list[i]["full_link"] = "https://d.tube/v/"+list[i]["_id"];
+      }
+      var original_requests = [];
+      list.forEach(element => {
+        if(! originaldtubers.includes(element.author)) {
+          original_requests.push(element);
+        }
+      });
+    }
+    console.log(JSON.stringify(original_requests));
+  }
   if(options.moments) {
-    var agg2 = filter("DTubeGo-Moments", options);
-    console.log(JSON.stringify(await fetcher(agg2, config)));
+    var agg2 = await filter("DTubeGo-Moments", options);
+    console.log(JSON.stringify(await link_fetcher(agg2, config)));
   }
   if(options.tag != undefined) {
-    var agg3 = filter(options.tag, options);
-    console.log(JSON.stringify(await fetcher(agg3, config)));
+    var agg3 = await filter(options.tag, options);
+    console.log(JSON.stringify(await link_fetcher(agg3, config)));
   }
 }
 
@@ -90,6 +112,7 @@ const program = new Command();
 program.option("-t, --tag <tag>", 'List videos tagged with \"tag\". It should be a string.');
 program.option("-1, --onelove", "List recent videos tagged \"onelove\" or \"onelovedtube\"", false);
 program.option("-m, --moments", "List recent DTubeGo moment videos", false);
+program.option("-o, --original", "Look for videos tagged #originaldtuber from non whitelisted users", false);
 program.option("-d, --days <N>", "How old the video can be? In days, default 5", 5);
 
 program.parse(process.argv);
